9e643a4e79a7af3c2e9b26699c0c1ecc11cda3cf
Nitirajsinh Solanki <solankin@go.stockton.edu>
2020-04-04 08:16:34 -0400

    latest version v2

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,4 @@
+
 def intersection(list1, list2):
     intersec=[]
     for x in list1:
@@ -6,76 +7,219 @@ def intersection(list1, list2):
                 intersec.append(x)
     return intersec
 
-def rowAvailibilty(numbers , aval):
+def rowAvailibilty(numbers , avalibilty):
     row_num=1
     for row in numbers:
         col_num=1
         avalible=[1,2,3,4,5,6,7,8,9]
         for digit in row:
             if digit !=0:
-                avalible.remove(digit)       
+                if digit in avalible:
+                    avalible.remove(digit)       
         for digit in row:
             if digit == 0:
-                aval[(row_num, col_num)] = avalible
+                avalibilty[(row_num, col_num)] = avalible
             else:
-                aval[(row_num, col_num)] = digit
+                avalibilty[(row_num, col_num)] = digit
             col_num +=1
         row_num += 1
-    return aval
+    return avalibilty
 
-def colAvailibilty(numbers, aval):
+def colAvailibilty(numbers, avalibilty):
     col_num=1
     for col_spot in range(1,10):
         row_num=1
         avalible=[1,2,3,4,5,6,7,8,9]
         for row_spot in range(1,10):
             if numbers[row_spot-1][col_spot-1] != 0:
-                avalible.remove(numbers[row_spot-1][col_spot-1])
+                if numbers[row_spot-1][col_spot-1] in avalible:
+                    avalible.remove(numbers[row_spot-1][col_spot-1])
         for row_spot in range(1,10):
             if numbers[row_spot-1][col_spot-1] == 0 :
                 avalibilty[(row_num, col_num)]= intersection(avalible,avalibilty[(row_num, col_num)])
             row_num += 1
         col_num += 1
-    return aval
+    return avalibilty
 
-def propigate(numbers, aval):
-    aval = rowAvailibilty(numbers, aval)
-    aval = colAvailibilty(numbers, aval)
-    for entry in aval:
+def cellAvailibilty(numbers, avalibilty):
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(0,3):
+        for col in range(0,3):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+        if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+
+    for row in range(0,3):
+        for col in range(3,6):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+        if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(0,3):
+        for col in range(6,9):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(3,6):
+        for col in range(0,3):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(3,6):
+        for col in range(3,6):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(3,6):
+        for col in range(6,9):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(6,9):
+        for col in range(0,3):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(6,9):
+        for col in range(3,6):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    avalible=[1,2,3,4,5,6,7,8,9]
+    keyset=[]
+    for row in range(6,9):
+        for col in range(6,9):
+            if numbers[row][col] != 0:
+                avalible.remove(numbers[row][col])
+                keyset.append((row+1,col+1))
+    for keys in keyset:
+         if isinstance(avalibilty[keys],list):
+            avalibilty[keys]=intersection(avalibilty[keys],avalible)
+    return avalibilty
+
+def finishCheck(numbers):
+    for rows in numbers:
+        for number in rows:
+            if number == 0:
+                return True    
+    return False
+
+def removeVisited(avalibilty, visited):
+    for spots in visited.keys():
+        if isinstance(avalibilty[spots],list):
+            intersec = intersection(avalibilty[spots], visited[spots])
+            for temp in intersec:
+                avalibilty[spots].remove(temp)
+    return avalibilty
+
+def propigate(numbers, avalibilty, visited):
+    avalibilty = rowAvailibilty(numbers, avalibilty)
+    avalibilty = colAvailibilty(numbers, avalibilty)
+    avalibilty = cellAvailibilty(numbers, avalibilty)
+    avalibilty = removeVisited(avalibilty, visited)
+    for entry in avalibilty:
         if isinstance(entry, list):
             if len(entry) == 0:
-                #back track
-                return null
-            else:
-                return aval
+                #backtrack(numbers, past_states)
+               return 0;
+    return avalibilty
 
-def _rowcheck(row, colum, numbers, aval):
-       
+def _rowcheck(row, numbers):
+       counter = 0
+       for col in range (0,9):
+           if numbers[row-1][col] == 0:
+               counter += 1
+       return counter
 
-def choose(numbers, aval):
-    least_constraind=[]
+def _colcheck(col, numbers):
+    counter = 0
+    for row in range(0,9):
+        if numbers[row][col-1] == 0:
+            counter +=1
+    return counter
+
+def choose(numbers, avalibilty, visited):
+    least_constrained=[]
     smallest=100
-    for spots in aval.keys():
-        if isinstance(aval[spots], list):
-            if len(aval[spots]) < smallest:
-                least_constraind.clear()                
-                least_constraind.append(spots)
-                smallest = len(aval[spots])
+    for spots in avalibilty.keys():
+        if isinstance(avalibilty[spots], list):
+            if len(avalibilty[spots]) < smallest:
+                least_constrained.clear()                
+                least_constrained.append(spots)
+                smallest = len(avalibilty[spots])
+            if len(avalibilty[spots]) == smallest:
+                least_constrained.append(spots)
     if smallest == 1:
         for spots in least_constrained:
-           for x,y in spots:
-               numbers[x][y] = aval[spots][0]
+            numbers[spots[0]][spots[1]] = avalibilty[spots][0]
     most_constraining=[]
     contrained=0
-    for spots in least_constraind:
-        
+    for spots in least_constrained:
+        current_contrained = _rowcheck(spots[0], numbers)+ _colcheck(spots[1], numbers)
+        if current_contrained > contrained:
+            most_constraining.clear
+            most_constraining.append(spots)
+        if current_contrained == contrained:
+            most_constraining.append(spots)
+
+    picked_spot = most_constraining[0]
+    value = avalibilty[picked_spot][0]
+    if picked_spot in visited:
+        visited[picked_spot].append(value)
+    else:
+        visited[picked_spot]=[value]
+    
+    numbers[picked_spot[0]][picked_spot[1]]=value
+    return numbers
+
+def backtrack(numbers, visited, tracked):
+    
     return numbers
 
 print('enter file name')
 file=open('s01a.txt','r')
 lines = file.readlines()
 numbers=[]
-past_states=[]
+visited={}
+tracked={}
 
 #initial run
 for x in lines:
@@ -88,10 +232,9 @@ for x in lines:
 
 #check
 print(numbers)
-
+print('needed change for commit')
 #solving star
 avalibilty={}
-avalibilty = rowAvailibilty(numbers,avalibilty)
-print(avalibilty)
-
-avalibilty = propigate(numbers,avalibilty)
+while finishCheck(numbers):
+    avalibilty=propigate(numbers, avalibilty, visited)
+    numbers= choose(numbers, avalibilty, visited)
